b684ee86abbf66e75eee729995cede2f176e3f97
Amit Anand <dropmail.amit@gmail.com>
2026-05-03 03:16:05 +0530

    Initial commit: @primitivekit/astro v0.0.1 with Button component

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @primitivekit/astro
+ * Version: 0.0.1
+ * 
+ * Primitive Astro components with zero hardcoded values.
+ * Fully customizable via CSS variables.
+ */
+
+export { default as Button } from './components/Button/Button.astro';
+
+export const version = '0.0.1';
