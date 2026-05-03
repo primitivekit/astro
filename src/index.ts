2812a6407453438f6762bb879d06e33432b9ce12
Amit Anand <dropmail.amit@gmail.com>
2026-05-03 11:03:12 +0530

    Add Input component and bump version to 0.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 /**
  * @primitivekit/astro
- * Version: 0.0.1
+ * Version: 0.0.2
  * 
  * Primitive Astro components with zero hardcoded values.
  * Fully customizable via CSS variables.
  */
 
 export { default as Button } from './components/Button/Button.astro';
+export { default as Input } from './components/Input/Input.astro';
 
-export const version = '0.0.1';
+export const version = '0.0.2';
